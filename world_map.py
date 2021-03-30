f7222be1f737d110ac86a60f4e52cafada0ed9ad
JakeThomson <jakethomson0@gmail.com>
2021-03-30 20:17:35 +0100

    Fixed a few bugs from data source table changes.

--- world_map.py
+++ world_map.py
@@ -17,11 +17,11 @@ def update_csv():
     temp_df = pd.read_csv('world_data.csv', index_col="CODE", engine="python")
     codes = pd.read_csv("country_codes.csv", index_col="COUNTRY")
 
-    for i, row in enumerate(table_rows[1:len(table_rows) - 1]):
+    for i, row in enumerate(table_rows[9:len(table_rows) - 8], 9):
         if i == 0:
             continue
         table_cells = table_rows[i].findAll("td")
-        country = table_cells[0].next.strip()
+        country = table_cells[1].next.next
         if country == "":
             try:
                 country = table_cells[0].findAll("a")[0].next
@@ -32,24 +32,16 @@ def update_csv():
         country = country.replace("N.", "North")
         country = country.replace("S.", "South")
 
-        cases = table_cells[1].next
+        cases = table_cells[2].next
         cases = int(cases.replace(",", ""))
 
-        deaths = table_cells[3].next
+        deaths = table_cells[4].next
         if not deaths == " ":
             deaths = int(deaths.replace(",", ""))
         else:
             deaths = 0
 
-        recoveries = table_cells[5].next
-        if not recoveries == " ":
-            recoveries = int(recoveries.replace(",", ""))
-        else:
-            recoveries = 0
-
-        active = cases - deaths - recoveries
-
-        if country == "" or country == "Channel Islands":
+        if country == "" or country == "Channel Islands" or country == "Caribbean Netherlands" or country == "Diamond Princess" or country.lower() == "ms zaandam":
             continue
         else:
             if country in codes.index:
@@ -62,7 +54,10 @@ def update_csv():
                         codes.loc[country, "CODE"] = code
                         codes.to_csv("country_codes.csv")
                         break
-                    except ValueError:
+                    except LookupError as e:
+                        print(e)
+                    except ValueError as e:
+                        print(e)
                         break
                     except requests.exceptions.ConnectionError:
                         print(f"Error: {e}")
@@ -71,51 +66,31 @@ def update_csv():
         try:
             old_cases = temp_df.loc[code, "CASES"]
             old_deaths = temp_df.loc[code, "DEATHS"]
-            old_recoveries = temp_df.loc[code, "RECOVERIES"]
-            old_active = temp_df.loc[code, "ACTIVE"]
             if isinstance(old_cases, str):
                 old_cases = int((temp_df.loc[code, "CASES"]).replace(",", ""))
             if isinstance(old_deaths, str):
                 old_deaths = int((temp_df.loc[code, "DEATHS"]).replace(",", ""))
-            if isinstance(old_recoveries, str):
-                old_recoveries = int((temp_df.loc[code, "RECOVERIES"]).replace(",", ""))
-            if isinstance(old_active, str):
-                old_active = int((temp_df.loc[code, "ACTIVE"]).replace(",", ""))
 
-            if cases - old_cases == 0:
-                temp_df.loc[code, "NEW CASES"] = " "
-            else:
-                temp_df.loc[code, "NEW CASES"] = " (+" + format(cases - old_cases, ",") + ")"
+            if type(cases) != int or type(old_cases) != int:
+                print(type(cases), type(old_cases))
+            temp_df.loc[code, "NEW CASES"] = cases - old_cases
 
             if deaths - old_deaths == 0:
                 temp_df.loc[code, "NEW DEATHS"] = " "
             else:
                 temp_df.loc[code, "NEW DEATHS"] = " (+" + format(deaths - old_deaths, ",") + ")"
 
-            if recoveries - old_recoveries == 0:
-                temp_df.loc[code, "NEW RECOVERIES"] = " "
-            else:
-                temp_df.loc[code, "NEW RECOVERIES"] = " (+" + format(recoveries - old_recoveries, ",") + ")"
-
-            if active - old_active == 0:
-                temp_df.loc[code, "NEW ACTIVE"] = " "
-            elif active - old_active > 0:
-                temp_df.loc[code, "NEW ACTIVE"] = " (+" + format(active - old_active, ",") + ")"
-            else:
-                temp_df.loc[code, "NEW ACTIVE"] = " (" + format(active - old_active, ",") + ")"
-
             temp_df.loc[code, "CASES"] = format(cases, ",")
             temp_df.loc[code, "DEATHS"] = format(deaths, ",")
-            temp_df.loc[code, "RECOVERIES"] = format(recoveries, ",")
-            temp_df.loc[code, "ACTIVE"] = format(active, ",")
-            if not active == 0:
-                temp_df.loc[code, "ACTIVE LOG"] = math.log(active)
             if not cases == 0:
                 temp_df.loc[code, "MORTALITY RATE"] = str(round(deaths/cases*100, 2)) + "%"
             else:
                 temp_df.loc[code, "MORTALITY RATE"] = "0"
             temp_df.loc[code, "LOGS"] = math.log(cases)
         except KeyError as e:
+            pass
+        except ValueError as e:
+            print(e)
             pass
 
     temp_df.to_csv("world_data.csv")
@@ -127,15 +102,14 @@ for col in df.columns:
     df[col] = df[col].astype(str)
 
 df["text"] = df["COUNTRY"] + "<br>" + \
-             "Total Cases: " + df["CASES"] + df["NEW CASES"] + "<br>" + \
+             "Total Cases: " + df["CASES"] + "<br>" + \
+             "New Cases: " + df["NEW CASES"] + "<br>" + \
              "Total Deaths: " + df["DEATHS"] + df["NEW DEATHS"] + "<br>" + \
-             "Total Recoveries: " + df["RECOVERIES"] + df["NEW RECOVERIES"] + "<br>" + \
              "Mortality Rate: " + df["MORTALITY RATE"]
 
 df["text2"] = df["COUNTRY"] + "<br>" + \
              "Active Cases: " + df["ACTIVE"] + df["NEW ACTIVE"] + "<br>" + \
              "Total Deaths: " + df["DEATHS"] + df["NEW DEATHS"] + "<br>" + \
-             "Total Recoveries: " + df["RECOVERIES"] + df["NEW RECOVERIES"] + "<br>" + \
              "Mortality Rate: " + df["MORTALITY RATE"]
 
 
@@ -180,6 +154,6 @@ if len(sys.argv) > 1 and sys.argv[1].lower() == "update":
     fig.show()
 
 elif sys.argv[1].lower() == "active":
-    fig = create_fig("ACTIVE LOG", "text2", "greens", "Active cases (log(x))")
-    update_fig(fig, "Active COVID-19 cases")
+    fig = create_fig("LOGS", "text", "reds", "Total cases (log(x))")
+    update_fig(fig, "Total COVID-19 cases")
     fig.show()
